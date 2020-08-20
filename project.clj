8b7788f390e48ec025efd4e634637c304d48cf13
Twan Coenraad <tcoenraad@users.noreply.github.com>
2020-08-20 12:53:19 +0200

    Add request and person support

--- project.clj
+++ project.clj
@@ -1,18 +1,9 @@
-(defproject circleci/rollcage
-  (format "1.0.%s" (or (System/getenv "CIRCLE_BUILD_NUM")
-                       "0-SNAPSHOT"))
+(defproject nedap/rollcage "1.0.0"
   :description "A Clojure client for Rollbar"
-  :url "http://github.com/circleci/rollcage"
+  :url "http://github.com/nedap/rollcage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["releases" {:url "https://clojars.org/repo"
-                              :username :env/clojars_username
-                              :password :env/clojars_password
-                              :sign-releases false}]
-                 ["snapshots" {:url "https://clojars.org/repo"
-                               :username :env/clojars_username
-                               :password :env/clojars_password
-                               :sign-releases false}]]
+  :signing {:gpg-key "releases-staffingsolutions@nedap.com"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.8.1"]
                  [clj-http "2.0.0"]
